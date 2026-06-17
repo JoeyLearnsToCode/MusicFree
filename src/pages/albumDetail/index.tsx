71bd3ee8c1699d747755ef046cb3093ff15fef41
JoeyLearnsToCode <zilin.wang.1024@gmail.com>
2026-06-17 15:11:28 +0800

    feat(albumDetail): add star/unstar button using album hooks

--- src/pages/albumDetail/index.tsx
+++ src/pages/albumDetail/index.tsx
@@ -3,6 +3,8 @@ import useAlbumDetail from "./hooks/useAlbumMusicList";
 import { useParams } from "@/core/router";
 import MusicSheetPage from "@/components/musicSheetPage";
 import { useI18N } from "@/core/i18n";
+import Album, { useAlbumIsStarred } from "@/core/album";
+import Toast from "@/utils/toast";
 
 export default function AlbumDetail() {
     const { albumItem: originalAlbumItem } = useParams<"album-detail">();
@@ -10,9 +12,24 @@ export default function AlbumDetail() {
         useAlbumDetail(originalAlbumItem);
     const { t } = useI18N();
 
+    const isAlbumStarred = useAlbumIsStarred(albumItem);
+    const handleStarPress = async () => {
+        if (!albumItem) return;
+        if (!isAlbumStarred) {
+            await Album.starAlbum(albumItem);
+            Toast.success(t("toast.hasStarredAlbum"));
+        } else {
+            await Album.unstarAlbum(albumItem);
+            Toast.success(t("toast.hasUnstarredAlbum"));
+        }
+    };
+
     return (
         <MusicSheetPage
-            navTitle={t("common.sheet")}
+            canStar
+            isStarred={isAlbumStarred}
+            onStarPress={handleStarPress}
+            navTitle={t("common.album")}
             sheetInfo={albumItem}
             state={requestStateCode}
             onRetry={getAlbumDetail}
